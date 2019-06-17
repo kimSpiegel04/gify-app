44aac0d88e02a6f6e91ee407502e4f113390a0bf
Kimberly Spiegel <k.spiegel72@gmail.com>
2019-06-17 18:30:46 -0400

    click to animate/still works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,37 +25,49 @@ function displayGifs(){
         //the rest
         for(var i=0;i<10;i++){
             console.log(response.data[i].images.fixed_height_still.url);
-            $('.display-gifs').prepend(`<img class='topic' src='${response.data[i].images.fixed_height_still.url}'><span class='rate'>${response.data[i].rating}</span>`);
+            $('.display-gifs').prepend(`<img class='topic gif' src='${response.data[i].images.fixed_height_still.url}' data-still='${response.data[i].images.fixed_height_still.url}' data-animate='${response.data[i].images.fixed_height.url}' data-state='still'><span class='rate'>${response.data[i].rating}</span>`);
         }
+
+            $(".gif").on("click", function() {
+                var state = $(this).attr("data-state");
+                // console.log(state);
+                if (state === "still") {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                }
+            });
     });
 }
 
-function addNavLinks() {
-    $('.dropdown-menu').empty();
-    
-    for (var i=0;i<topics.length;i++){
-        console.log(topics[i]);
-        var a=$('<a>');
-        a.addClass('dropdown-item');
-        a.attr('data-name', topics[i]);
-        a.text(topics[i]);
-        $('.dropdown-menu').append(a);
+    function addNavLinks() {
+        $('.dropdown-menu').empty();
+        
+        for (var i=0;i<topics.length;i++){
+            console.log(topics[i]);
+            var a=$('<a>');
+            a.addClass('dropdown-item');
+            a.attr('data-name', topics[i]);
+            a.text(topics[i]);
+            $('.dropdown-menu').append(a);
+        }
     }
-}
 
-$('#add-search').on('click',function(event){
-    event.preventDefault();
-    var topic=$('#add-gif').val().trim();
-    topics.push(topic);
+    $('#add-search').on('click',function(event){
+        event.preventDefault();
+        var topic=$('#add-gif').val().trim();
+        topics.push(topic);
+        addNavLinks();
+        console.log(this);
+        // $('.nav-link').addClass('.search-clicked');
+        // dropdown.classList.add('.search-clicked');
+        // $('#add-search').text('Search');
+    });
+
+
+    // var dropdown = document.querySelectorAll('.dropdown');
+    $(document).on('click','.dropdown-item', displayGifs);
+
     addNavLinks();
-    console.log(this);
-    // $('.nav-link').addClass('.search-clicked');
-    // dropdown.classList.add('.search-clicked');
-    // $('#add-search').text('Search');
-});
-
-
-// var dropdown = document.querySelectorAll('.dropdown');
-$(document).on('click','.dropdown-item', displayGifs);
-
-addNavLinks();
